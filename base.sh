0cef78f25cbd371bbf2f10dc10fe88ff49924bcd
Robert Constantinescu <91455746+RCX777@users.noreply.github.com>
2022-08-07 15:49:50 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -8,7 +8,7 @@ ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
 hwclock --systohc
 
 # Generating locale
-sed -i "/en_US.UTF-8/s/^#//g" /etc/locale.gen
+sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
@@ -22,43 +22,47 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 ### Package installation
 
 # Some base packages (replace intel-ucode with amd-ucode if needed)
-pacman -Syu intel-ucode base-devel git
+pacman -Syu --needed intel-ucode base-devel git xdg-user-dirs
 
 # Text editor (please don't kill me vim users)
-pacman -S micro
+pacman -S --needed micro
 
 # Networking (networkmanager, firewalld with nftables, openssh)
-pacman -S networkmanager
+pacman -S --needed networkmanager
 systemctl enable NetworkManager
-pacman -S nftables iptables-nft firewalld ipset
+pacman -S --needed nftables iptables-nft firewalld ipset
 systemctl enable firewalld
-pacman -S openssh
+pacman -S --needed openssh
 systemctl enable sshd
 
 # Sound (pipewire)
-pacman -S sof-firmware 
-pacman -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack
+pacman -S --needed sof-firmware 
+pacman -S --needed alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack
 
-# Bluetooth (bluez)
-pacman -S bluez bluez-utils
+# Bluetooth (bluez) 
+pacman -S --needed bluez bluez-utils
 systemctl enable bluetooth
 
+# Printing (cups)
+pacman -S --needed cups
+systemctl enable cups
+
 # Bootloader (refind)
-pacman -S refind
+pacman -S --needed refind 
 refind-install
 
 # NTFS file system support
-pacman -S ntfs-3g
+pacman -S --needed ntfs-3g
 
 # SSD weekly TRIM using fstrim.timer
-pacman -S util-linux
+pacman -S --needed util-linux
 systemctl enable fstrim.timer
 
 # GPU drivers (nvidia proprietary)
-pacman -S nvidia nvidia-utils nvidia-settings nvidia-prime
+pacman -S --needed nvidia nvidia-utils nvidia-settings nvidia-prime
 
 # User & passwords
-pacman -S sudo
+pacman -S --needed sudo
 echo root:password | chpasswd
 useradd -m user
 echo user:password | chpasswd
